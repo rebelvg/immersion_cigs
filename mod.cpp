428b8979c263eaa9d889c4a2e4fc93325a60962e
rebelvg <johnhartigan@ya.ru>
2015-11-03 14:14:06 +0200

    Proper folders.

---
+++ mod.cpp
@@ -0,0 +1,14 @@
+name = "Murshun Cigs 1.12";
+picture = "murshun_cigs.paa";
+actionName = "Website";
+action = "http://podkolpakom.net/";
+description = "Murshun Cigs";
+logo = "murshun_cigs.paa";
+logoOver = "murshun_cigs.paa";
+tooltip = "Murshun Cigs";
+tooltipOwned = "";
+overview = "Cigarette script turned into a mod";
+author = "rebel";
+overviewPicture = "murshun_cigs.paa";
+overviewText = "Murshun Cigs";
+overviewFootnote = "";
