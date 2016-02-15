7b7ad92d2bd931a36635c03ba7870cc90d02d72c
Luigi <luigium@outlook.fr>
2016-02-15 08:50:09 +0100

    Adding system of translations and French translations

--- addons/murshun_cigs/config.cpp
+++ addons/murshun_cigs/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches
 	class murshun_cigs
 	{
 		units[] = {"murshun_cigs_matchesItem", "murshun_cigs_lighterItem"};
-        weapons[] = {"murshun_cigs_matches", "murshun_cigs_lighter"};
+		weapons[] = {"murshun_cigs_matches", "murshun_cigs_lighter"};
 		requiredVersion = 1;
 		requiredAddons[] = {"ace_main", "ace_common", "ace_interaction", "ace_interact_menu", "ewk_cigs"};
 		version = "1.13";
@@ -20,7 +20,7 @@ class CfgVehicles
 			class ACE_Equipment {
 				class murshun_cigs_start_cig
 				{
-					displayName = "Light Up a Cig";
+					displayName = "$STR_murshun_cigs_start_cig";
 					condition = "((goggles _player) in murshun_cigs_cigsArray) and (!(_player getVariable ['murshun_cigs_cigLitUp', false])) and ((_player getVariable ['murshun_cigs_cigTime', 0]) <= 330) and (alive _player)";
 					statement = "[_player, _player] spawn murshun_cigs_fnc_start_cig";
 					showDisabled = 0;
@@ -29,7 +29,7 @@ class CfgVehicles
 				};
 				class murshun_cigs_stop_cig
 				{
-					displayName = "Put Out a Cig";
+					displayName = "$STR_murshun_cigs_stop_cig";
 					condition = "((goggles _player) in murshun_cigs_cigsArray) and ((_player getVariable ['murshun_cigs_cigLitUp', false])) and (alive _player)";
 					statement = "[_player] spawn murshun_cigs_fnc_stop_cig";
 					showDisabled = 0;
@@ -44,7 +44,7 @@ class CfgVehicles
 			class ACE_Head {
 				class murshun_cigs_start_someones_cig
 				{
-					displayName = "Light Up a Cig";
+					displayName = "$STR_cigs_start_someones_cig";
 					condition = "((goggles _target) in murshun_cigs_cigsArray) and (!(_target getVariable ['murshun_cigs_cigLitUp', false])) and (alive _target)";
 					statement = "[_target, _player] spawn murshun_cigs_fnc_start_cig_MP";
 					showDisabled = 0;
@@ -57,34 +57,34 @@ class CfgVehicles
 	class Item_Base_F;
 	
 	class murshun_cigs_matchesItem: Item_Base_F {
-        scope = 2;
-        scopeCurator = 2;
-        displayName = "Matches";
-        author = "rebel / facel";
-        vehicleClass = "Items";
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "$STR_murshun_cigs_matches";
+		author = "rebel / facel";
+		vehicleClass = "Items";
 		
-        class TransportItems {
-            class murshun_cigs_matches {
+		class TransportItems {
+			class murshun_cigs_matches {
 				name = "murshun_cigs_matches";
 				count = "1";
 			};
-        };
-    };
+		};
+	};
 	
 	class murshun_cigs_lighterItem: Item_Base_F {
-        scope = 2;
-        scopeCurator = 2;
-        displayName = "Lighter";
-        author = "rebel / facel";
-        vehicleClass = "Items";
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "$STR_murshun_cigs_lighter";
+		author = "rebel / facel";
+		vehicleClass = "Items";
 		
-        class TransportItems {
-            class murshun_cigs_lighter {
+		class TransportItems {
+			class murshun_cigs_lighter {
 				name = "murshun_cigs_lighter";
 				count = "1";
 			};
-        };
-    };
+		};
+	};
 };
 
 class CfgWeapons {
@@ -92,34 +92,34 @@ class CfgWeapons {
 	class InventoryItem_Base_F;
 	
 	class murshun_cigs_matches: ACE_ItemCore {
-        author = "rebel / facel";
-        scope = 2;
+		author = "rebel / facel";
+		scope = 2;
 		scopeCurator = 2;
-        displayName = "Matches";
-        descriptionShort = "Only 10 matches per box.";
-        model = "\murshun_cigs\matches.p3d";
-        picture = "\murshun_cigs\UI\gear_murshun_cigs_matches_x_ca";
+		displayName = "$STR_murshun_cigs_matches";
+		descriptionShort = "Only 10 matches per box.";
+		model = "\murshun_cigs\matches.p3d";
+		picture = "\murshun_cigs\UI\gear_murshun_cigs_matches_x_ca";
 		
-        class ItemInfo: InventoryItem_Base_F {
-            mass = 1;
+		class ItemInfo: InventoryItem_Base_F {
+			mass = 1;
 			model = "\murshun_cigs\matches.p3d";
-        };
-    };
+		};
+	};
 	
 	class murshun_cigs_lighter: ACE_ItemCore {
-        author = "rebel / facel";
-        scope = 2;
+		author = "rebel / facel";
+		scope = 2;
 		scopeCurator = 2;
-        displayName = "Lighter";
-        descriptionShort = "Unlimited lighting possibilities.";
-        model = "\murshun_cigs\lighter.p3d";
-        picture = "\murshun_cigs\UI\gear_murshun_cigs_lighter_x_ca";
+		displayName = "$STR_murshun_cigs_lighter";
+		descriptionShort = "Unlimited lighting possibilities.";
+		model = "\murshun_cigs\lighter.p3d";
+		picture = "\murshun_cigs\UI\gear_murshun_cigs_lighter_x_ca";
 		
-        class ItemInfo: InventoryItem_Base_F {
-            mass = 1;
+		class ItemInfo: InventoryItem_Base_F {
+			mass = 1;
 			model = "\murshun_cigs\lighter.p3d";
-        };
-    };
+		};
+	};
 };
 
 class CfgGlasses
