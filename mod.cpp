e0be50cda3fe94163fb451864bbbca996c2ea3af
Rebel <johnhartigan@ya.ru>
2017-03-26 20:34:09 +0300

    no message

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,5 @@
-name = "Murshun Cigs";
+name = "Immersion Cigs";
 picture = "mod.paa";
 actionName = "Website";
 action = "https://github.com/rebelvg";
-overview = "Cigarette script turned into a mod.";
 author = "Rebel / Facel";
