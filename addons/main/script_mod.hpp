afb2d4533156846db63ae063245d1ba890cf6e26
OverlordZorn <dennisAzorn@gmail.com>
2024-11-18 03:45:47 +0100

    added main and template addon

---
+++ addons/main/script_mod.hpp
@@ -0,0 +1,7 @@
+#define MAINPREFIX z
+#define PREFIX cigs
+
+#define PREFIX_BEAUTIFIED Immersion Cigs
+
+
+#include "script_version.hpp"
---
+++ addons/main/script_version.hpp
@@ -0,0 +1,10 @@
+#define MAJOR 1
+#define MINOR 5
+#define PATCH 0
+#define BUILD 0
+
+
+// #define VERSION MACROS
+#define VERSION MAJOR.MINOR
+#define VERSION_AR MAJOR,MINOR,PATCH,BUILD
+#define VERSION_STR MAJOR##.##MINOR##.##PATCH##.##BUILD
