a6e89418e93aac16ca3993271ff0a958d50694ad
OverlordZorn <dennisAzorn@gmail.com>
2024-11-22 13:15:25 +0100

    remove comment

--- addons/anim/script_component.hpp
+++ addons/anim/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT anim
-#define COMPONENT_BEAUTIFIED Animations       // This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Animations
 
 
 #include "\z\cigs\addons\main\script_mod.hpp"
--- addons/core/script_component.hpp
+++ addons/core/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT core
-#define COMPONENT_BEAUTIFIED Core       // This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Core
 
 
 #include "\z\cigs\addons\main\script_mod.hpp"
