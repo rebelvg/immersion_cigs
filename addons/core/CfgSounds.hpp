a19b95832470c074dad3dc63d053b568affc7565
OverlordZorn <dennisAzorn@gmail.com>
2024-11-27 04:44:54 +0100

    sucker sounds

--- addons/core/CfgSounds.hpp
+++ addons/core/CfgSounds.hpp
@@ -17,6 +17,43 @@ class CfgSounds
         sound[] = {PATH_TO_ADDON_3(data,sounds,lighter_01.ogg), 2, 1, 15};
         titles[] = {};
     };
+
+
+    class PVAR(eat_01)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_01.ogg), 2, 1, 15};
+        titles[] = {};
+    };
+    class PVAR(eat_02)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_02.ogg), 2, 1, 15};
+        titles[] = {};
+    };
+    class PVAR(eat_03)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_03.ogg), 2, 1, 15};
+        titles[] = {};
+    };
+    class PVAR(eat_04)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_04.ogg), 2, 1, 15};
+        titles[] = {};
+    };
+    class PVAR(eat_05)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_05.ogg), 2, 1, 15};
+        titles[] = {};
+    };
+    class PVAR(eat_06)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_06.ogg), 2, 1, 15};
+        titles[] = {};
+    };
+    class PVAR(eat_07)
+    {
+        sound[] = {PATH_TO_ADDON_3(data,sounds,eat_07.ogg), 2, 1, 15};
+        titles[] = {};
+    };
 };
 
 
